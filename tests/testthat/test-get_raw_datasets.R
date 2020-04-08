6acffe3bdc098df7da165c33a05ddfafde289113
Nicolas Roelandt <roelandtn.pro@gmail.com>
2020-04-08 10:32:50 +0200

    get_raw_datasets works with a numeric vector

--- tests/testthat/test-get_raw_datasets.R
+++ tests/testthat/test-get_raw_datasets.R
@@ -22,5 +22,30 @@ test_that("get_raw_datasets works with one year", {
 })
 
 
+test_that("get_raw_datasets works with a numeric vector", {
+
+  temp_dir <- tempdir()
+  get_raw_datasets(year = c(2018, 2016), path = tempdir())
+
+  files <- list.files(path = temp_dir, pattern = ".csv")
+
+  files <- tibble::tribble(
+    ~file,   ~size,
+    "caracteristiques-2018.csv", 4696158,
+    "caracteristiques_2016.csv", 4616433,
+    "lieux-2018.csv", 2838996,
+    "lieux_2016.csv", 3148830,
+    "usagers-2018.csv", 5403979,
+    "usagers_2016.csv", 5705950,
+    "vehicules-2018.csv", 3756297,
+    "vehicules_2016.csv", 3872943
+  )
+  # Test if the downloaded files has the expected size
+  expect_equal(
+    fs::dir_info(temp_dir, glob = "*.csv") %>%
+      dplyr::transmute(file = basename(path), size = as.numeric(size)),
+    files)
+})
+
 
 
